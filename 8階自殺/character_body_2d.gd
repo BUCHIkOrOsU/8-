d97e3d4bd77734b481ae9443c63efa0534c14d59
fun <1lk6071h@komazawa-u.ac.jp>
2026-08-11 18:04:03 +0900

    仮UIがたぶんできた

--- 8階自殺/character_body_2d.gd
+++ 8階自殺/character_body_2d.gd
@@ -1,38 +1,96 @@
 extends CharacterBody2D
 var speed = 700
-var direction = "migi" #方向
+var direction = "migi" 
 var friction = 1500
 @onready var camera: Camera2D = $Camera2D
 
-# Called when the node enters the scene tree for the first time.
+var is_choosing = false
+var choice_index = 0            
+var choices = ["はい", "いいえ"]
+
+@onready var choice_ui: Control = $"../CanvasLayer/choiceUI"
+@onready var question_label: Label = $"../CanvasLayer/choiceUI/VBoxContainer/PanelContainer/questionlabel"
+@onready var choice_labels: Array = [
+	$"../CanvasLayer/choiceUI/VBoxContainer/HBoxContainer/PanelContainer/yeslabel",
+	$"../CanvasLayer/choiceUI/VBoxContainer/HBoxContainer/PanelContainer2/nolabel"
+]
+
 func _ready() -> void:
 	camera.make_current()
-	#camera.zoom = Vector2(1.5, 1.5)
 	camera.position_smoothing_enabled = false
 	camera.offset = Vector2.ZERO
+	choice_ui.visible = false
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 @warning_ignore("unused_parameter")
 func _physics_process(delta: float) -> void:
-	
+
+	if Input.is_action_just_pressed("ui_down") and not is_choosing:
+		open_choice()
+		return
+
+	if is_choosing:
+		handle_choice_input()
+		velocity = Vector2.ZERO
+		move_and_slide()
+		return
+
 	if Input.is_action_pressed("ui_right"):
 		velocity.x = speed
 		$"first_person".play("migi_muki")
-		direction="migi" #向いている方向
-	
+		direction = "migi"
+
 	elif Input.is_action_pressed("ui_left"):
 		velocity.x = -speed
 		$"first_person".play("hidari_muki")
-		direction="hidari"
-	
-	else :
+		direction = "hidari"
+
+	else:
 		velocity.x = move_toward(velocity.x, 0, friction * delta)
 		if velocity.x == 0:
 			if direction == "migi":
-				$"first_person".play("tomari_migi_muki")	
+				$"first_person".play("tomari_migi_muki")
 			elif direction == "hidari":
 				$"first_person".play("tomari_hidari_muki")
 
-
-
 	move_and_slide()
+
+
+func open_choice() -> void:
+	is_choosing = true
+	choice_index = 0
+	question_label.text = "飛び降りますか？"
+	choice_ui.visible = true
+	update_choice_display()
+
+
+func handle_choice_input() -> void:
+	if Input.is_action_just_pressed("ui_left"):
+		choice_index = (choice_index - 1 + choices.size()) % choices.size()
+		update_choice_display()
+	elif Input.is_action_just_pressed("ui_right"):
+		choice_index = (choice_index + 1) % choices.size()
+		update_choice_display()
+	elif Input.is_action_just_pressed("ui_accept"):
+		confirm_choice()
+
+
+func update_choice_display() -> void:
+	for i in choice_labels.size():
+		if i == choice_index:
+			choice_labels[i].modulate = Color(1, 1, 0)
+		else:
+			choice_labels[i].modulate = Color(1, 1, 1)
+
+
+func confirm_choice() -> void:
+	if choice_index == 0:
+		print("はいが選ばれました")
+	else:
+		print("いいえが選ばれました")
+
+	close_choice()
+
+
+func close_choice() -> void:
+	is_choosing = false
+	choice_ui.visible = false
